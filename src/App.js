90b7bb09333ac7b5fc591a1f23b7ac9fddfd8e23
ankur598 <ankursingh.as278@gmail.com>
2021-12-21 15:58:50 +0530

    doing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import Slider from "./components/Slider";
+import Products from "./components/Products";
+import Footer from "./components/Footer";
+import Advertisement from "./components/Advertisements";
+import SignIn from "./components/modals/SignIn";
+import { DisplayAuth } from "./contexts/SliderDisplay";
+import { useContext } from "react";
+import SignUp from "./components/modals/SignUp";
 function App() {
+  let { state, setState } = useContext(DisplayAuth);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Search />
+      <Slider />
+      <Products />
+      <Advertisement />
+      <Footer />
+      {/* <SignIn state={state} setState={setState} /> */}
+      <SignUp state={state} setState={setState} />
     </div>
   );
 }
